e332ee9747469327c6ddb7139611d10fc7d4f0e1
Preston Davies <pdavies88@gmail.com>
2023-11-17 23:00:07 -0500

    Home, Category, and Product Pages

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -29,18 +29,18 @@ export default async function Hero() {
           <div className='relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0'>
             <Image
               src={urlFor(data.image1).url()}
-              alt='Great Photo'
+              alt='Hero Photo 1'
               className='h-full w-full object-cover object-center'
-              priority
               width={500}
               height={500}
+              priority
             />
           </div>
 
           <div className='overflow-hidden rounded-lg bg-gray-100 shadow-lg'>
             <Image
               src={urlFor(data.image2).url()}
-              alt='Great Photo'
+              alt='Hero Photo 2'
               className='h-full w-full object-cover object-center'
               width={500}
               height={500}
