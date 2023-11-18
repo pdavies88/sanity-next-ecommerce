fdc672831c1dc283636ff6775632177bf4873ede
Preston Davies <pdavies@Prestons-Air-M2.fios-router.home>
2023-11-17 23:00:07 -0500

    Home, Category, and Product Pages

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,17 +1,16 @@
 import Link from 'next/link';
-import { simplifiedProduct } from '../interfaces';
 import { client } from '../lib/sanity';
-import { ArrowRight } from 'lucide-react';
 import Image from 'next/image';
+import { simplifiedProduct } from '../interfaces';
 
-async function getData() {
-  const query = `*[_type == "product"][0...4] | order(_createdAt desc) {
+async function getData(category: string) {
+  const query = `*[_type == "product" && category->name == "${category}"] {
         _id,
-        price,
-        name,
-        "slug": slug.current,
-        "categoryName": category->name,
-        "imageUrl": images[0].asset->url
+          "imageUrl": images[0].asset->url,
+          price,
+          name,
+          "slug": slug.current,
+          "categoryName": category->name
       }`;
 
   const data = await client.fetch(query);
@@ -19,23 +18,22 @@ async function getData() {
   return data;
 }
 
-export default async function Newest() {
-  const data: simplifiedProduct[] = await getData();
+export const dynamic = 'force-dynamic';
+
+export default async function CategoryPage({
+  params,
+}: {
+  params: { category: string };
+}) {
+  const data: simplifiedProduct[] = await getData(params.category);
 
   return (
     <div className='bg-white'>
-      <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
+      <div className='mx-auto max-w-2xl px-4 sm:px-6  lg:max-w-7xl lg:px-8'>
         <div className='flex justify-between items-center'>
           <h2 className='text-2xl font-bold tracking-tight text-gray-900'>
-            Our Newest products
+            Our Products for {params.category}
           </h2>
-
-          <Link className='text-primary flex items-center gap-x-1' href='/all'>
-            See All{' '}
-            <span>
-              <ArrowRight />
-            </span>
-          </Link>
         </div>
 
         <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
